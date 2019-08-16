bd9e291d3644b659a681acd8a2961288ac6d55ce
bishalDores <lazarisalive@gmail.com>
2019-08-16 07:49:05 +0600

    moving to redux advanced

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,7 +16,7 @@ class BurgerBuilder extends Component{
     state = {
         // ingredients:null,
         // totalPrice: 4,
-        purchasable:false,
+        // purchasable:false,
         purchasing:false,
         loading:false,
         error:false
@@ -29,7 +29,7 @@ class BurgerBuilder extends Component{
             .reduce(((sum,el)=>{
                 return sum + el
             }),0);
-        this.setState({purchasable: sum > 0})
+        return sum > 0;
     }
     // addIngredientHandler = (type) => {
     //     const oldCount = this.state.ingredients[type];
@@ -69,39 +69,8 @@ class BurgerBuilder extends Component{
         this.setState({purchasing:false})
     };
     purchaseContinueHandler = () => {
-        // alert("you will continue");
-        // this.setState({loading:true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Bishal Peter Dores',
-        //         address: {
-        //             country: 'Bangladesh',
-        //             zipCode: '1215',
-        //             street: 'East Razabazar'
-        //         },
-        //         email: 'bishalpd@gmail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // };
-        // axios.post('/orders.json',order)
-        //     .then(response => {
-        //         this.setState({loading:false,purchasing:false})
-        //     })
-        //     .catch(e =>{
-        //         this.setState({loading:false,purchasing:false})
-        //     })
-        const queryParams = [];
-        for (let i in this.state.ingredients){
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&');
-        this.props.history.push({
-            pathname:'/checkout',
-            search:'?' + queryString
-        });
+
+        this.props.history.push('/checkout')
     };
     componentDidMount(){
         console.log(this.props.ings)
@@ -134,7 +103,7 @@ class BurgerBuilder extends Component{
                         ingredientRemoved = {this.props.onIngredientRemoved}
                         disabled = {disabledInfo}
                         price={this.props.totalPrice}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                     />
                 </Aux>
